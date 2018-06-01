af4c35ac08eb8460b6c7569cbb830453f6dff239
myafa <821koesnow@gmail.com>
2018-06-01 18:00:08 +0900

    node class defined

---
+++ linkedList.py
@@ -0,0 +1,26 @@
+class Node:
+    nodeNext = ''
+    objValue = ''
+    binHead = False
+    binTail = False
+    def __init__(self, objValue="",nodeNext='',binHead=False,binTail=False):
+        self.nodeNext = nodeNext
+        self.objValue = objValue
+	self.binHead = binHead
+	self.binTail = binTail
+    def getValue(self):
+ 	return self.objValue
+    def setValue(self, objValue):
+	self.objValue = objValue
+    def getNext(self):
+	return self.nodeNext
+    def setNext(self, nodeNext):
+  	self.nodeNext = nodeNext
+    def isHead(self):
+	return self.binHead
+    def isTail(self):
+	return self.binTail
+
+class SinglyLinkedList:
+    nodeHead = ''
+    pass 
