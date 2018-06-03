524f45679476bf25d764d635df77e68657f49110
myafa <821koesnow@gmail.com>
2018-06-03 17:12:01 +0900

    first SLL coded

--- linkedList.py
+++ linkedList.py
@@ -23,4 +23,38 @@ class Node:
 
 class SinglyLinkedList:
     nodeHead = ''
-    pass 
+    nodeTail = ''
+    size = 0
+    def __init__(self):
+ 	self.nodeTail = Node(binTail=True)
+	self.nodeHead = Node(binHead=True, nodeNext = self.nodeTail)
+    
+    def get(self, idxRetrieve):
+	nodeReturn = self.nodeHead
+ 	for itr in range(idxRetrieve +1):
+	    nodeReturn = nodeReturn.getNext()
+	return nodeReturn
+
+    def insertAt(self, objInsert, IdxInsert):
+	nodeNew = Node(objValue=objInsert)
+	nodePrev = self.get(IdxInsert -1)
+	nodeNext = nodePrev.getNext()
+	nodePrev.setNext(nodeNew)
+	nodeNew.setNext(nodeNext)
+	self.size = self.size +1
+
+    def removeAt(self, idxRemove):
+	nodePrev = self.get(idxRemove -1)
+	nodeNext = self.get(idxRemove +1)
+	nodePrev.setNext(nodeNext)
+	self.size -=1
+
+    def printStatus(self):
+	nodeCurrent = self.nodeHead
+	while nodeCurrent.getNext().isTail() == False:
+	    nodeCurrent = nodeCurrent.getNext()
+	    print nodeCurrent.getValue(),
+	print
+
+    def getSize(self):
+	return self.size
